882c6619430c3e7d7a7e6423fa7daaec8eeee11c
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2021-08-06 11:37:38 -0500

    Update EventManager.java
    No destruction by lava/fire, no nugget crafting

--- src/com/gmail/creepycucumber1/elytraplus/EventManager.java
+++ src/com/gmail/creepycucumber1/elytraplus/EventManager.java
@@ -1,18 +1,22 @@
 package com.gmail.creepycucumber1.elytraplus;
 
+import com.gmail.creepycucumber1.elytraplus.Items.AmethystElytra;
+import com.gmail.creepycucumber1.elytraplus.Items.Elytra;
 import com.gmail.creepycucumber1.elytraplus.Items.Fragment;
+import com.gmail.creepycucumber1.elytraplus.Items.NetheriteElytra;
 import org.bukkit.*;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
+import java.util.Objects;
 import java.util.Random;
 
 public class EventManager implements Listener {
@@ -73,19 +77,67 @@ public class EventManager implements Listener {
         }
     }
 
-    //handles elytra deployment for boom elytra
+    //cancels combustion of netherite elytra
     @EventHandler
-    public static void onElytraDeploy(EntityToggleGlideEvent e) {
-        if (e.getEntity() instanceof Player) {
-            Player p = (Player) e.getEntity();
-            if (p.isSneaking() && !p.isGliding()) {
-                if (p.getInventory().getChestplate() != null && p.getInventory().getChestplate().getItemMeta().hasCustomModelData() &&
-                        p.getInventory().getChestplate().getItemMeta().getCustomModelData() == 3) { //boom
-                    p.setVelocity(p.getLocation().getDirection().multiply(1.07));
-                    Location l = p.getLocation().clone();
-                    p.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, l, 10, 1, 1, 1);
-                    p.getWorld().playSound(l, Sound.ENTITY_DRAGON_FIREBALL_EXPLODE, SoundCategory.MASTER, 3, 2);
+    public void onEntityCombust(EntityCombustEvent e) {
+        if(e.getEntity() instanceof Item) {
+            Item i = (Item) e.getEntity();
+            if(i.getItemStack().getType().equals(Material.ELYTRA) && i.getItemStack().hasItemMeta() &&
+                    i.getItemStack().getItemMeta().hasCustomModelData() && i.getItemStack().getItemMeta().getCustomModelData() == 2) { //netherite
+                i.setInvulnerable(true);
+            }
+        }
+    }
+    @EventHandler
+    public void onEntityDamageByLava(EntityDamageEvent e) {
+        if(e.getEntity() instanceof Item) {
+            if(e.getCause().equals(EntityDamageEvent.DamageCause.LAVA) || e.getCause().equals(EntityDamageEvent.DamageCause.FIRE_TICK)) {
+                Item i = (Item) e.getEntity();
+                if(i.getItemStack().getType().equals(Material.ELYTRA) && i.getItemStack().hasItemMeta() &&
+                        i.getItemStack().getItemMeta().hasCustomModelData() && i.getItemStack().getItemMeta().getCustomModelData() == 2) { //netherite
+                    i.setInvulnerable(true);
+                    i.setFireTicks(0);
                 }
+            }
+        }
+    }
+
+
+    //custom item verification in custom recipes (prevents nuggets from being viable ingredients)
+
+    @EventHandler
+    public void onPrepareFragmentCraft(PrepareItemCraftEvent e) {
+        if (Objects.equals(e.getInventory().getResult(), Fragment.fragment)) {
+            CraftingInventory inv = e.getInventory();
+            ItemStack[] items = inv.getMatrix();
+
+            //if any of the 6 side crafting slots aren't essence (CustomModelData 1): cancel
+            if (!items[0].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1
+                    || !items[3].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1
+                    || !items[6].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1
+                    || !items[2].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1
+                    || !items[5].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1
+                    || !items[8].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 1) {
+                e.getInventory().setResult(new ItemStack(Material.AIR));
+            }
+        }
+    }
+    @EventHandler
+    public void onPrepareAnyElytraCraft(PrepareItemCraftEvent e) {
+        if (Objects.equals(e.getInventory().getResult(), AmethystElytra.aelytra) ||
+                Objects.equals(e.getInventory().getResult(), NetheriteElytra.nelytra) ||
+                Objects.equals(e.getInventory().getResult(), Elytra.elytra)) {
+            CraftingInventory inv = e.getInventory();
+            ItemStack[] items = inv.getMatrix();
+
+            //if any of the 6 side crafting slots aren't fragment (CustomModelData 2): cancel
+            if (!items[0].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2
+                    || !items[3].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2
+                    || !items[6].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2
+                    || !items[2].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2
+                    || !items[5].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2
+                    || !items[8].getItemMeta().hasCustomModelData() || items[0].getItemMeta().getCustomModelData() != 2) {
+                e.getInventory().setResult(new ItemStack(Material.AIR));
             }
         }
     }
