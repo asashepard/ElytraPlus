ac509994b704bf98fa6199a5b7472c871a7f2187
Asa Shepard <78510770+asashepard@users.noreply.github.com>
2021-08-12 15:55:41 -0500

    Create AmethystElytra.java

---
+++ src/main/java/com/gmail/creepycucumber1/elytraplus/Items/AmethystElytra.java
@@ -0,0 +1,42 @@
+package com.gmail.creepycucumber1.elytraplus.Items;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AmethystElytra {
+
+    public static ItemStack aelytra;
+
+    public static void init() { createaelytra(); }
+
+    private static void createaelytra() {
+        ItemStack item = new ItemStack(Material.ELYTRA);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.DARK_PURPLE + "Amethyst Elytra");
+        List<String> lore = new ArrayList<String>();
+        lore.add(ChatColor.WHITE + (ChatColor.ITALIC + "light and fast"));
+        meta.setLore(lore);
+
+        meta.setCustomModelData(1);
+
+        item.setItemMeta(meta);
+        aelytra = item;
+
+        ShapedRecipe aelytraR = new ShapedRecipe(NamespacedKey.minecraft("aelytra"), aelytra);
+        aelytraR.shape("FAF",
+                       "FAF",
+                       "FAF");
+        aelytraR.setIngredient('F', Fragment.fragment.getType()); aelytraR.setIngredient('A', Material.AMETHYST_CLUSTER);
+        try {
+            Bukkit.getServer().addRecipe(aelytraR);
+        } catch (IllegalStateException ignored) { }
+    }
+}
